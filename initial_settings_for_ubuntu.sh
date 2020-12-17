7adea2a8d05e76a16cfe9ad16cb672a8af7d0ceb
kazscape <kazscape@gmail.com>
2020-12-17 17:42:25 +0900

    Add shell script of Initial setting for Ubuntu

---
+++ initial_settings_for_ubuntu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Install required modules
+sudo apt-get install build-essential curl file git
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Make .bash_profile if not exists
+if [ ! -e ~/.bash_profile ]; then
+  touch ~/.bash_profile
+fi
+
+# Set environment variables 
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+STR="
+# Begin environment variables for Homebrew
+eval \$($(brew --prefix)/bin/brew shellenv)
+# End environment variables for Homebrew
+"
+test -r ~/.bash_profile && echo "${STR}" >> ~/.bash_profile
+echo "${STR}" >> ~/.profile
+brew --version
+
+# Install Ansible
+brew install ansible
+ansible --version
+
+# Reload shell
+exec $SHELL -l
