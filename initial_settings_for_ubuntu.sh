6141e5a29fbb9b9d587b10d0d8d3f09fa5067ace
kazscape <3782734+kazscape@users.noreply.github.com>
2022-10-13 13:34:54 +0900

    Update initial_settings_for_ubuntu.sh

--- initial_settings_for_ubuntu.sh
+++ initial_settings_for_ubuntu.sh
@@ -3,13 +3,18 @@
 # Install required modules
 sudo apt-get install -y \
 build-essential \
+libbz2-dev \
+libdb-dev \
+libreadline-dev \
 libffi-dev \
+libgdbm-dev \
+liblzma-dev \
+libncursesw5-dev \
+libsqlite3-dev \
 libssl-dev \
 zlib1g-dev \
-liblzma-dev \
-libbz2-dev \
-libreadline-dev \
-libsqlite3-dev \
+uuid-dev \
+tk-dev \
 git
 
 # Install Homebrew
